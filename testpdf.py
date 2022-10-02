9f9417dbcdfc0723a3bbef759455eeac07cf0333
Maquine <mcmaquine@gmail.com>
2022-10-01 21:11:12 -0400

    testing csv reading

--- testpdf.py
+++ testpdf.py
@@ -1,22 +1,23 @@
 import PyPDF2
 import tabula
-# pdf file object
-# you can find find the pdf file with complete code in below
-##pdfFileObj = open('librev.pdf', 'rb')
+import csv
 
-# pdf reader object
-##pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+try:
+    f = open('pdf.csv', 'x')
+    df = tabula.convert_into("librev.pdf", "pdf.csv", output_format="csv")
+except FileExistsError:
+    print ('File Already Exists')
 
-# number of pages in pdf
-##print(pdfReader.numPages)
-
-# a page object
-##pageObj = pdfReader.getPage(0)
-
-# extracting text from page.
-# this will print the text you can also save that into String
-
-##print(pageObj.extractText())
-
-df = tabula.convert_into("librev.pdf", "pdf.csv", output_format="csv")
 # in order to print first 5 lines of Table
+
+with open('pdf.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
